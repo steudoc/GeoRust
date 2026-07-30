09104d63e9483a3baa20f7e53e13af4245f07be0
Stefano Tallone <s307569@studenti.polito.it>
2026-07-30 17:10:45 +0200

    Implemented basic client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fmt::format, time::Duration};
+
+use common::{LoginRequest, LoginResponse, RegisterRequest, RegisterResponse};
+
+const SERVER_HTTP: &str = "http://127.0.0.1:3000";
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let http = reqwest::Client::new();
+
+    // registrazione
+    let username = "utente_demo".to_string();
+    let password = "password".to_string();
+
+    let register_resp = http
+        .post(format!("{SERVER_HTTP}/register"))
+        .json(&RegisterRequest {
+            username: username.clone(),
+            password: password.clone(),
+        })
+        .send()
+        .await?;
+
+    if register_resp.status().is_success() {
+        println!("Registration: OK");
+    } else {
+        println!(
+            "Registration: FAILED ({}) \nTrying login...",
+            register_resp.status()
+        )
+    }
+
+    // login
+    let login_resp: LoginResponse = http
+        .post(format!("{SERVER_HTTP}/login"))
+        .json(&LoginRequest {username, password})
+        .send()
+        .await?
+        .error_for_status()?
+        .json()
+        .await?;
+
+    println!("Login: OK, user_id = {}", login_resp.user_id);
+
+    Ok(())
 }
