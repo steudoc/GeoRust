b60ea97312e66136489b4e88588fc1891d905fdc
Stefano Tallone <s307569@studenti.polito.it>
2026-08-08 16:26:01 +0200

    Added first terminal interface for server. (To be improved)

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,6 @@
 mod state;
 mod auth;
+mod stats;
 
 use std::sync::Arc;
 use common::{WsClientMessage::{self, Text}, WsServerMessage};
@@ -23,6 +24,7 @@ use rand::Rng;
 use std::time::Duration;
 use tokio::time;
 use tokio::sync::mpsc;
+use tokio::io::{self, AsyncBufReadExt, BufReader};
 
 
 #[tokio::main]
@@ -41,7 +43,7 @@ async fn main() -> anyhow::Result<()> {
             )
         })?;
 
-    let state = AppState::new(pool);
+    let state = AppState::new(pool.clone());
 
     let app = Router::new()
         .route("/register", post(auth::register))
@@ -51,6 +53,56 @@ async fn main() -> anyhow::Result<()> {
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
     tracing::info!("Server running at http://0.0.0.0:3000");
+
+    
+    let db_for_admin = pool;
+    tokio::spawn(async move {
+        let stdin = io::stdin();
+        let mut reader = BufReader::new(stdin).lines();
+
+        println!("Admin console ready. Press 'help' for command menu.");
+
+        while let Ok(Some(line)) = reader.next_line().await {
+            let input = line.trim();
+            if input.is_empty() { continue; }
+            
+            let parts: Vec<&str> = input.split_whitespace().collect();
+            match parts[0] {
+                "stats" => {
+                    // e.g. stats <user_id> <day|week|month>
+                    if parts.len() == 3 {
+                        if let Ok(user_id) = parts[1].parse::<i64>() {
+                            let interval = parts[2];
+                            println!("Calculating stats for user {} in period {}...", user_id, interval);
+
+                            // chiamata alla funzione di analisi passandogli db_for_admin
+                            match stats::calculate_user_stats(&db_for_admin, user_id, interval).await {
+                                Ok(res) => {
+                                    println!("--------------------------------");
+                                    println!("User id:\t\t{}", user_id);
+                                    println!("Period:\t\t\t{}", res.period);
+                                    println!("Distance:\t\t{:.2}", res.distance);
+                                    println!("Total time:\t\t{:.2}", res.total_time);
+                                    println!("Total pause time:\t{:.2}", res.total_pause);
+                                    println!("Average velocity:\t{:.2}", res.avg_velocity);
+                                    println!("--------------------------------");
+                                }
+                                Err(e) => {
+                                    println!("Error in db data extraction: {}", e);
+                                }
+                            }
+                        }
+                    } else {
+                        println!("Usage: stats <user_id> <day|week|month>");
+                    }
+                },
+                "help" => println!("Commands available: \n\tstats <user_id> <day|week|month>\n\thelp"),
+                _ => println!("Invalid command"),
+            }
+        }
+    });
+
+
     axum::serve(listener, app).await?;
 
     Ok(())
